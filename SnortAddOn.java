8fea0b65fee93d2cf81d754151e5a60755700db5
Abdulellah <cs_saheel@hotmail.com>
2012-01-10 11:32:45 +0300

    this commit only describes an idea/principle of snort-redirector project, i cleaned some comments here

--- SnortAddOn.java
+++ SnortAddOn.java
@@ -181,7 +181,6 @@ if(snort_conf.exists()){
     }catch(Exception e){
         System.out.print("Exception!");
     }
-    //System.out.print("alert file exist");
 
 }
 }
@@ -224,7 +223,6 @@ if(alertF.exists()){
     forRegEx = line.split("[**] \\[");
     if(forRegEx[0].contains(" [**]")){
     forRegEx = forRegEx[0].split(":");
-    //if(forRegEx[1].matches("[0-9]+")){
     sid = forRegEx[1];
     for(int i=0; i<files.size();i++){
     for(int j=0; j<files.get(i).rules.size();j++){
@@ -234,11 +232,9 @@ if(alertF.exists()){
     }
     }
     }
-    //}
     }
     }
     else if(listed){
-    //if(true){
     if(line.contains(" -> ")){
     forRegEx = line.split(" -> ");
     forRegEx = forRegEx[1].split(":");
